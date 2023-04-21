b532e8a6feb7699d02d0a65249984a932e4bcd1c
Ignacio Carnero <nachocarnero77@gmail.com>
2023-04-21 14:03:13 -0300

    Acomodar carpetas Front, Back y Maqueta. Landing page con Angular y Boostrap

---
+++ front/ecommerce-ispc/src/app/shared/section/section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent {
+
+}
