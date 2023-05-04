9a6d8f94cfa2417f240417e0d4a518ce773eb6df
Ignacio Carnero <105066510+IgnacioCarnero@users.noreply.github.com>
2023-05-04 14:44:23 -0300

    Revert "Revert "Atorrez""

--- front/ecommerce-ispc/src/app/app.module.ts
+++ front/ecommerce-ispc/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+iimport { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
