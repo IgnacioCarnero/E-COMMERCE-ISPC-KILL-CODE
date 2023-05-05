38285f8f9f8c4b6a74f1c8ae3529efa25e7511d6
= <=>
2023-05-05 09:59:21 -0300

    subo mi nuevo componente y mi base de datos con visibilidad que proviene esto  de mi rama leonardo

--- front/ecommerce-ispc/src/app/pages/generador-recibo-de-sueldo/generador-recibo-de-sueldo.component.ts
+++ front/ecommerce-ispc/src/app/pages/generador-recibo-de-sueldo/generador-recibo-de-sueldo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// Acá declaramos las variables, constantes o funciones por ejemplo para la página web de generador de recibo de sueldo
 @Component({
   selector: 'app-generador-recibo-de-sueldo',
   templateUrl: './generador-recibo-de-sueldo.component.html',
