f37c7d0c824030ee3bfe2d1989b558b567028a75
= <=>
2023-05-06 21:59:13 -0300

    agregando mi página web a routing y agrego esilos

--- front/ecommerce-ispc/src/app/app-routing.module.ts
+++ front/ecommerce-ispc/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { PrivacidadComponent } from './pages/privacidad/privacidad.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ServiciosComponent } from './pages/home/servicios/servicios.component';
+import { GeneradorReciboDeSueldoComponent } from './pages/dashboard/generador-recibo-de-sueldo/generador-recibo-de-sueldo.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'privacidad', component: PrivacidadComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'servicios', component: ServiciosComponent },
+  { path: 'generador de recibo de sueldo', component: GeneradorReciboDeSueldoComponent}
 ];
 
 @NgModule({
