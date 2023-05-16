8c7bf112daa807191d8c28baf8d3a067ffb0e634
= <=>
2023-05-16 13:19:19 -0300

    Configuración de mysql

--- backend_django/home/settings.py
+++ backend_django/home/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EcommerceKillCode',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
