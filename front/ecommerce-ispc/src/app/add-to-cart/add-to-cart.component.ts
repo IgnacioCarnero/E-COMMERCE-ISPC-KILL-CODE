2f6439db2514f4d5de6132a83254dcc46874c225
Leo-Farran <leo.farran89@gmail.com>
2023-05-21 19:31:32 -0300

    Se aplican observables para manejo de eventos

--- front/ecommerce-ispc/src/app/add-to-cart/add-to-cart.component.ts
+++ front/ecommerce-ispc/src/app/add-to-cart/add-to-cart.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Servicio } from '../pages/servicios/servicios.component';
 import { CartService } from '../services/cart.service';
+import { EventService } from '../services/event.service';
+
 
 @Component({
   selector: 'app-add-to-cart',
@@ -10,10 +12,12 @@ import { CartService } from '../services/cart.service';
 export class AddToCartComponent {
   productosCarrito: Servicio[] = [];
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private eventService: EventService) {
     this.productosCarrito = this.cartService.getProductosCarrito();
   }
   eliminarDelCarrito(servicio: Servicio) {
     this.cartService.eliminarDelCarrito(servicio);
+    servicio.isInCart = false;
+    this.eventService.actualizarCarrito();
   }
 }
--- front/ecommerce-ispc/src/app/pages/servicios/servicios.component.ts
+++ front/ecommerce-ispc/src/app/pages/servicios/servicios.component.ts
@@ -1,25 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { EventService } from 'src/app/services/event.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css']
 })
-export class ServiciosComponent {
+export class ServiciosComponent implements AfterViewInit, OnDestroy{
   Personal: Servicio = new Servicio('Personal', '$20000');
+  PyME: Servicio = new Servicio('PyME', '$60000');
+  PremiumPyME: Servicio = new Servicio('PremiumPyME', '$100000');
+  
+  private carritoActualizadoSubscription!: Subscription;
+  constructor(private cartService: CartService, private eventService: EventService) { }
 
-  constructor(private cartService: CartService) { }
+  ngAfterViewInit() {
+    this.cambioDeBoton(this.Personal);
+    this.cambioDeBoton(this.PyME);
+    this.carritoActualizadoSubscription = this.eventService.carritoActualizado$.subscribe(() => {
+      this.cambioDeBoton(this.Personal);
+      this.cambioDeBoton(this.PyME);
+    });
+  }
+
+  ngOnDestroy() {
+    this.carritoActualizadoSubscription.unsubscribe();
+  }
 
   agregarAlCarrito(servicio: Servicio) {
     this.cartService.agregarAlCarrito(servicio);
-}
+    servicio.isInCart = true;
+  }
+
+  cambioDeBoton(servicio: Servicio): { class: string, text: string } {
+    if (servicio.isInCart) {
+      return { class: 'btn btn-success', text: 'Agregado al carrito' };
+    } else {
+      return { class: 'btn btn-primary', text: 'Agregar licencia mensual' };
+    }
+  }
+  
 }
 export class Servicio {
   nombre: string;
   precio: string;
+  isInCart: boolean;
   constructor(nombre: string, precio: string){
     this.nombre = nombre;
     this.precio = precio;
+    this.isInCart = false;
   }
 }
