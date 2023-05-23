ff938b3358f589cf8330bc9cdc45e94dfa7a7185
= <=>
2023-05-23 13:42:59 -0300

    Problema resuelta de levantamiento del servidor

--- backend_django/ABM_ISPC/wsgi.py
+++ backend_django/ABM_ISPC/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ABM_ISPC.settings')
 
 application = get_wsgi_application()
