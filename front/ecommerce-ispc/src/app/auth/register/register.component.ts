a92fd7b84a5fc26c89f6f568b1d99e70cd097b2f
Ignacio Carnero <nachocarnero77@gmail.com>
2023-06-03 11:55:34 -0300

    Realizo validaciones con FormsGroup de Login y Registro

--- front/ecommerce-ispc/src/app/auth/register/register.component.ts
+++ front/ecommerce-ispc/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  userRegister = new FormGroup ({
+    companynameregister: new FormControl('', Validators.pattern('[a-zA-Z ]*')),
+    usernameregister: new FormControl('', [Validators.required,Validators.email]),
+    userpasswordregister: new FormControl('',[Validators.required,Validators.minLength(10)]),
+    confirmpasswordregister: new FormControl('',[Validators.required,Validators.minLength(10)]),
+
+
+  })
+
+  get companynameregister() {
+    return this.userRegister.get('companynameregister')
+  }
+
+  get usernameregister() {
+    return this.userRegister.get('usernameregister')
+  }
+
+  get userpasswordregister() {
+    return this.userRegister.get('userpasswordregister')
+  }
+
+  get confirmpasswordregister() {
+    return this.userRegister.get('confirmpasswordregister')
+  }
 }
