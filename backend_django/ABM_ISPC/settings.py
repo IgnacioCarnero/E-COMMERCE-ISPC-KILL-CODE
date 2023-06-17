25e6feb902d79897bca645360d137c51995fcb2a
Leonardo Cesar <leo.farran89@gmail.com>
2023-06-16 21:18:24 -0300

    Metodo eliminar y modificar
    Se completa CRUD con método eliminar y modificar para la sección empleados.

--- backend_django/ABM_ISPC/settings.py
+++ backend_django/ABM_ISPC/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,10 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:4200"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:4200",]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'ABM_ISPC.urls'
