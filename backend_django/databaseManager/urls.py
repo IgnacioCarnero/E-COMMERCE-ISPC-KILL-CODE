46413f7ae613eaf6ed7f86b4b784951441d5e649
Leonardo Cesar <leo.farran89@gmail.com>
2023-06-19 01:40:56 -0300

    Pedido
    Se desarrolla view, serializador y se modifica modelo de pedido para recibir pago de tarjeta de crédito.

--- backend_django/databaseManager/urls.py
+++ backend_django/databaseManager/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path('listar-reclamos/', ListarReclamoView.as_view(), name='listar-reclamos'),
     path('modificar-reclamo/<int:id_recla>/', ModificarReclamoView.as_view(), name='modificar-reclamo'),    
     path('eliminar-reclamo/<int:id_recla>/', EliminarReclamoView.as_view(), name='eliminar-reclamo'),   
+    path('hacer-pedido/', CrearPedidoView.as_view(), name='hacer-pedido'),
+    path('ver-pedido/', VerPedidoView.as_view(), name='ver-pedido'),
 ]
