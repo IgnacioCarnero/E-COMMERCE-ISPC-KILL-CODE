fef0d49309c8175c95232e8b6b87870f2b03cfd0
Ignacio Carnero <nachocarnero77@gmail.com>
2023-06-29 23:48:18 -0300

    Modificaciones en componentes Login y Registro

--- front/ecommerce-ispc/src/app/auth/login/login.component.ts
+++ front/ecommerce-ispc/src/app/auth/login/login.component.ts
@@ -20,8 +20,13 @@ export class LoginComponent {
     this.dynamicIdSuffix++;
   }
 
+  get errorMessage():string {
+    return this.authService.errorMessage
+  }
+
   submited = false
   loggedInUserEmail: string = '';
+
   //validaciones
 
   userLogin = new FormGroup({
@@ -29,17 +34,17 @@ export class LoginComponent {
     userpassword : new FormControl('', [Validators.required, Validators.minLength(8)])
 
   });
-  
+
   submitData(): void {
     this.submited = true;
-  
+
     if (this.userLogin.invalid) {
       return;
     }
-  
+
     const email = this.userLogin.value.email;
     const password = this.userLogin.value.userpassword;
-  
+
     if(email && password){
       this.authService.login(email, password);
     }
