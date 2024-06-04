59dcbd489aa66634ff023b36f2121ba611ceca60
tomiLencina <tomilencina10@gmail.com>
2024-06-04 17:08:01 -0300

    actualizando back, agregando servicio y css de cart

--- backend_django/ABM_ISPC/settings.py
+++ backend_django/ABM_ISPC/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommercekillcodeproject',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '12Python34*',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
